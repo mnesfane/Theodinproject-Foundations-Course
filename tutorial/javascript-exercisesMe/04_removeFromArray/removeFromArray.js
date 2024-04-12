4f54fae43e8cb5e0b837eff5eea75a81fa8acc61
mnesfane <abdessalamnesfane@gmail.com>
2024-04-12 00:02:52 +0000

    04

--- tutorial/javascript-exercisesMe/04_removeFromArray/removeFromArray.js
+++ tutorial/javascript-exercisesMe/04_removeFromArray/removeFromArray.js
@@ -1,10 +1,14 @@
-const removeFromArray = function(numArr, num) {
-    let newArr;
-    for(numArr of numA){
-        if (numA != num)
-            newArr.push(numA)
+const removeFromArray = function(numArr, ...args) {
+    let newArray = [];
+    for(numA of numArr){
+        if (!args.includes(numA))
+            newArray.push(numA)
+        // for(let i = 0; i < numArr; i++){
+
+        // }
     }
+    return newArray;
 };
-removeFromArray()
+// console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
