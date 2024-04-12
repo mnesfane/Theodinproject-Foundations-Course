52b5d1766c41304669d342ae3e51a8778d01c298
mnesfane <abdessalamnesfane@gmail.com>
2024-04-12 01:16:21 +0000

    06 done

--- tutorial/javascript-exercisesMe/06_leapYears/leapYears.js
+++ tutorial/javascript-exercisesMe/06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // if (year % 400 == 0)
+    //     return true
+    // else if (year % 4 == 0 && year % 100 != 0 )
+    //     return true
+    // return false
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
 };
-
+console.log(leapYears(1800))
 // Do not edit below this line
 module.exports = leapYears;
