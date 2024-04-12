9f6cf1a5856e4136ec04058db6cb2675bfa4c68a
mnesfane <abdessalamnesfane@gmail.com>
2024-04-12 00:31:08 +0000

    05 done

--- tutorial/javascript-exercisesMe/05_sumAll/sumAll.js
+++ tutorial/javascript-exercisesMe/05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const isNumber = function(arg){
+    return typeof arg === 'number';
 };
+
+const sumAll = function(startNumb, lastNum) {
+    let s = 0
+    if (startNumb < 0 || lastNum < 0 || !isNumber(startNumb) || !isNumber(lastNum)){
+        return "ERROR"
+    }
+    if(startNumb > lastNum){
+        let n = startNumb
+        startNumb = lastNum
+        lastNum = n
+    }
+    for (; startNumb <= lastNum; startNumb++){
+        s += startNumb
+    }
+    return s
+};
+// console.log(sumAll(10, "90"))
 
 // Do not edit below this line
 module.exports = sumAll;
