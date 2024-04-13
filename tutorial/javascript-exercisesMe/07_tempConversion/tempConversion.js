e84d6bd2fc7576df0f8bfea9cc97e04bd46dc287
mnesfane <abdessalamnesfane@gmail.com>
2024-04-13 23:47:33 +0000

    07 done

--- tutorial/javascript-exercisesMe/07_tempConversion/tempConversion.js
+++ tutorial/javascript-exercisesMe/07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5/9);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * (9/5)) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
