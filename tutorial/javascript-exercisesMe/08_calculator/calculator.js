138dd3be766a549a1bd13741ce0a349514e895a1
mnesfane <abdessalamnesfane@gmail.com>
2024-04-14 00:42:04 +0000

    08 done

--- tutorial/javascript-exercisesMe/08_calculator/calculator.js
+++ tutorial/javascript-exercisesMe/08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, value) => accumulator + value, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1
+  for (let i = 2; i < num; i++){
+    result *= i 
+  }
 };
 
 // Do not edit below this line
