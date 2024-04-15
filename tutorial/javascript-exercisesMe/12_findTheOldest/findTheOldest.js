0a7717c39b0bc2e2d3aac45da5db1e6083ba0fbc
mnesfane <abdessalamnesfane@gmail.com>
2024-04-15 23:51:49 +0100

    12 done

--- tutorial/javascript-exercisesMe/12_findTheOldest/findTheOldest.js
+++ tutorial/javascript-exercisesMe/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const getAge= function(person){
+    if(person.yearOfDeath)
+        return person.yearOfDeath - person.yearOfBirth
+    else{
+        let date = new Date()
+        return date.getFullYear() - person.yearOfBirth
+    }
+}
 
+const findTheOldest = function(people) {
+    return people.reduce(function(oldestPers, currentPers){
+        const currentPersAge = getAge(currentPers)
+        const oldestPersAge = getAge(oldestPers)
+        return currentPersAge > oldestPersAge ? currentPers : oldestPers
+    })
 };
 
+// console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
