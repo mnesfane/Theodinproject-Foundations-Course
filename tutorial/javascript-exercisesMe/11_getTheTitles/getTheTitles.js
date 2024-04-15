f07b2dd4333ea82d93516b98ff6a14441c9001ad
mnesfane <abdessalamnesfane@gmail.com>
2024-04-15 16:54:09 +0100

    10 v1 v2 done

--- tutorial/javascript-exercisesMe/11_getTheTitles/getTheTitles.js
+++ tutorial/javascript-exercisesMe/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookTitles = []
+    for (book of books){
+        bookTitles.push(book.title)
+    }
+    return bookTitles
 };
 
+const getTheTitles2 = function(books) {
+    return books.map(function(book){
+        return book.title
+    })
+}
+
+console.log(getTheTitles2([{  title: 'Book',  author: 'Name'},{  title: 'Book2',  author: 'Name2'}]))
 // Do not edit below this line
 module.exports = getTheTitles;
