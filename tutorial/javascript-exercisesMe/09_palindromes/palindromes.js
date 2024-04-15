d0dfc9fa26fb1e51d37aa8e07f6914a5bbdc4aec
mnesfane <abdessalamnesfane@gmail.com>
2024-04-15 16:35:14 +0100

    09 v2 done

--- tutorial/javascript-exercisesMe/09_palindromes/palindromes.js
+++ tutorial/javascript-exercisesMe/09_palindromes/palindromes.js
@@ -1,3 +1,4 @@
+// version1
 const isAlphanumeric = function(char){
     return /[a-zA-Z0-9]/.test(char)
 }
@@ -7,7 +8,7 @@ const palindromes = function (text) {
     let onlyAlphaNumText = "";
     for(let i = 0; i < text.length; i++){
         if (isAlphanumeric(text[i]))
-            onlyAlphaNumText += text[i]
+        onlyAlphaNumText += text[i]
     }
     let bool = true
     let j = 0
@@ -19,7 +20,14 @@ const palindromes = function (text) {
     }
     return bool
 };
-console.log(palindromes("Animal loots foliated detail of stool lamina."))
+
+// version2
+const palindromes2 = function (text) {
+    let AlphaNumStr = text.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
+    return AlphaNumStr === AlphaNumStr.split('').reverse().join('')
+}
+
+// console.log(palindromes2("tacos"))
 
 // Do not edit below this line
 module.exports = palindromes;
