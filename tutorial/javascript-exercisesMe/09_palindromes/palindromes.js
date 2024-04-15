be112a4cfb209e52d5409db5da2543a54108e8c6
mnesfane <abdessalamnesfane@gmail.com>
2024-04-15 16:04:14 +0100

    09 done

--- tutorial/javascript-exercisesMe/09_palindromes/palindromes.js
+++ tutorial/javascript-exercisesMe/09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const isAlphanumeric = function(char){
+    return /[a-zA-Z0-9]/.test(char)
+}
 
+const palindromes = function (text) {
+    text = text.toLowerCase();
+    let onlyAlphaNumText = "";
+    for(let i = 0; i < text.length; i++){
+        if (isAlphanumeric(text[i]))
+            onlyAlphaNumText += text[i]
+    }
+    let bool = true
+    let j = 0
+    let len = onlyAlphaNumText.length - 1
+    while(j < len){
+        if (onlyAlphaNumText[j] != onlyAlphaNumText[len - j])
+            bool = false
+        j++
+    }
+    return bool
 };
+console.log(palindromes("Animal loots foliated detail of stool lamina."))
 
 // Do not edit below this line
 module.exports = palindromes;
