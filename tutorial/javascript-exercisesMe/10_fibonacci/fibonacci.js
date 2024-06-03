7ce6f788f8145b0f167f1bc9c90efc957703ff01
mnesfane <abdessalamnesfane@gmail.com>
2024-06-03 16:18:42 +0100

    fibonacci done

--- tutorial/javascript-exercisesMe/10_fibonacci/fibonacci.js
+++ tutorial/javascript-exercisesMe/10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+let n = 8
+// memoization
+// let fibonacciCache = new Array(n + 1)
+function  fibonacci(n){
+    if(n < 0)
+        console.log("OOPS")
+    if(n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2)
 };
 
+// console.log(fibonacci(8))
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
