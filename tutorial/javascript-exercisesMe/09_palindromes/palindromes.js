ba21a3861135a281ebc50981884165b9cf82dfe4
mnesfane <abdessalamnesfane@gmail.com>
2024-09-15 16:41:17 +0100

    palind done

--- tutorial/javascript-exercisesMe/09_palindromes/palindromes.js
+++ tutorial/javascript-exercisesMe/09_palindromes/palindromes.js
@@ -1,33 +1,14 @@
-// version1
-const isAlphanumeric = function(char){
-    return /[a-zA-Z0-9]/.test(char)
-}
-
-const palindromes = function (text) {
-    text = text.toLowerCase();
-    let onlyAlphaNumText = "";
-    for(let i = 0; i < text.length; i++){
-        if (isAlphanumeric(text[i]))
-        onlyAlphaNumText += text[i]
+const palindromes = function (str) {
+    let len = str.length - 1;
+    for (let i = 0; i <= len; i++){
+        if (str[i] !== str[len]){
+        return false}
+        len--;
     }
-    let bool = true
-    let j = 0
-    let len = onlyAlphaNumText.length - 1
-    while(j < len){
-        if (onlyAlphaNumText[j] != onlyAlphaNumText[len - j])
-            bool = false
-        j++
-    }
-    return bool
+    return true
 };
 
-// version2
-const palindromes2 = function (text) {
-    let AlphaNumStr = text.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
-    return AlphaNumStr === AlphaNumStr.split('').reverse().join('')
-}
-
-// console.log(palindromes2("tacos"))
+// console.log(palindromes('tacos'))
 
 // Do not edit below this line
 module.exports = palindromes;
