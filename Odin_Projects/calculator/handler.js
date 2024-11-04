b5a1be162c26c40119ae741f8caa1a9fe8222941
mnesfane <abdessalamnesfane@gmail.com>
2024-11-04 15:00:00 +0100

    commented hundler.js and trying github dates(11-04)

--- Odin_Projects/calculator/handler.js
+++ Odin_Projects/calculator/handler.js
@@ -1,42 +1,42 @@
-import * as utils from './utils.js'
-import * as app from './app.js'
+// import * as utils from './utils.js'
+// import * as app from './app.js'
 
-export const v = 33
-export const handleDivision = function() {
-    // logic for division
-};
+// export const v = 33
+// export const handleDivision = function() {
+//     // logic for division
+// };
 
-export const handleMultiplication = function() {
-    // logic for multiplication
-};
+// export const handleMultiplication = function() {
+//     // logic for multiplication
+// };
 
-export const handleMince = function() {
-    // logic for mince
-};
+// export const handleMince = function() {
+//     // logic for mince
+// };
 
-export const handlePlus = function() {
-    // logic for plus
-};
+// export const handlePlus = function() {
+//     // logic for plus
+// };
 
-export const handleEqual = function() {
-    // logic for equal
-};
-// ------------- handleAC functions
+// export const handleEqual = function() {
+//     // logic for equal
+// };
+// // ------------- handleAC functions
 
 
-export const handleAC = function() {
-    // alert('fhfh')
-    // alert(nb)
-    utils.removeActiveIfExists()
-    if(app.nbr !== 0){
-    }
+// export const handleAC = function() {
+//     // alert('fhfh')
+//     // alert(nb)
+//     utils.removeActiveIfExists()
+//     if(app.nbr !== 0){
+//     }
 
-};
+// };
 
-export const handlePlusMinus = function() {
-    // logic for equal
-};
+// export const handlePlusMinus = function() {
+//     // logic for equal
+// };
 
-export const handlePercentage = function() {
-    // logic for equal
-};
+// export const handlePercentage = function() {
+//     // logic for equal
+// };
